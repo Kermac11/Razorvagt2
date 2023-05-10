6795e9c6587939e2e8abc77dcd61bce2b2fb1a2b
Kermac11 <73934167+Kermac11@users.noreply.github.com>
2023-05-10 10:40:35 +0200

    Working on Type catalog

---
+++ Razorvagt2/Interfaces/IAssignmentTypeCatalog.cs
@@ -0,0 +1,20 @@
+﻿using Razorvagt2.Models;
+namespace Razorvagt2.Interfaces
+{
+    public interface IAssignmentTypeCatalog
+    {
+        Task<List<string>> GetAllTypes();
+
+        Task<string> GetTypeString(int id);
+
+        Task<bool> CreateAssignmentType(string type);
+
+        Task<bool> CreateTag(Assignment assignment, string tag);
+
+        Task<bool> DeleteTag(int id);
+
+        Task<string> DeleteAssignmentType();
+
+        Task<List<Assignment>> GetAssignmentsOfType(string type);
+    }
+}
---
+++ Razorvagt2/Services/AssignmentTypeCatalog.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Data.SqlClient;
+using Razorvagt2.Interfaces;
+using Razorvagt2.Models;
+
+namespace Razorvagt2.Services
+{
+    public class AssignmentTypeCatalog:Connection, IAssignmentTypeCatalog
+    {
+        private string GetAllSql = "SELECT * from AssignmentType";
+        private string GetTypeStringSql = "SELECT * from AssignmentType WHERE AssignmentType_ID = @ID";
+        private string GetAllTagFromTypeSql = "SELECT * from AssignmentTypeTag WHERE AssignmentType_ID = @ID";
+        private string GetAllTagFromAssignmentSql = "SELECT * from AssignmentTypeTag WHERE Assignment_ID = @ID";
+        private string DeleteTypeSql = "DELETE from AssignmentType WHERE AssignmentType_ID = @ID";
+        private string DeleteTagSql = "DELETE from AssignmentTypeTag WHERE AssignmentTypeTag_ID = @ID";
+        private string InsertTypeSql = "INSERT into AssignmentType values(@Type)";
+        private string InsertTagSql = "INSERT intro AssignmentTypeTag values (@A,@Type)";
+
+
+        /*CREATE TABLE [dbo].[AssignmentType] (
+    [AssignmentType_ID] INT          IDENTITY (1, 1) NOT NULL,
+    [Type]              VARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([AssignmentType_ID] ASC)*/
+
+        /*CREATE TABLE [dbo].[AssignmentTypeTag] (
+    [AssignmentTypeTag_ID] INT IDENTITY (1, 1) NOT NULL,
+    [AssignmentType_ID]    INT NOT NULL,
+    [Assignment_ID]        INT NOT NULL,
+    CONSTRAINT [PK_AssignmentTypeTag] PRIMARY KEY CLUSTERED ([AssignmentTypeTag_ID] ASC),
+    CONSTRAINT [AssignmentType_ID] FOREIGN KEY ([AssignmentType_ID]) REFERENCES [dbo].[AssignmentType] ([AssignmentType_ID]),
+    CONSTRAINT [Assignment_ID] FOREIGN KEY ([Assignment_ID]) REFERENCES [dbo].[Assignment] ([Assignment_ID])*/
+
+        public AssignmentTypeCatalog(IConfiguration configuration) : base(configuration)
+        {
+
+        }
+
+        public Task<bool> CreateAssignmentType(string type)
+        {
+            throw new NotImplementedException();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand(InsertTypeSql,connection))
+                {
+
+                }
+            }
+        }
+
+        public Task<bool> CreateTag(Assignment assignment, string tag)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> DeleteAssignmentType()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteTag(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<string>> GetAllTypes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Assignment>> GetAssignmentsOfType(string type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetTypeString(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
